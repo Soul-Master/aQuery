6a297ad14fe9d3f540b54bde7cfd44cfa261c3b3
Soul_Master <Soul_Master@live.com>
2015-12-08 00:34:48 +0700

    Add last, first filter.

---
+++ aQuery/FirstFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Windows.Automation;
+
+namespace aQuery
+{
+    public class FirstFilter : ICustomFilter
+    {
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public IEnumerable<AutomationElement> Filter(IEnumerable<AutomationElement> elements)
+        {
+            return new List<AutomationElement> { elements.First() };
+        }
+    }
+}
